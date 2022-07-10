b9eca5ca4ce931bd2a35a8f32f61eab833acd65c
burntcustard <burntcustard@gmail.com>
2022-07-10 23:55:20 +0100

    Make regpack work

--- build.js
+++ build.js
@@ -1,21 +1,25 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { minify as minifyJs } from "terser";
 import { minify as minifyHtml } from 'html-minifier';
+import { cmdRegPack } from 'regpack';
 
 const options = {
   toplevel: true,
   compress: {
     passes: 2,
-    unsafe: true,
-    unsafe_arrows: true,
-    unsafe_comps: true,
-    unsafe_math: true,
+    // unsafe: true,
+    // unsafe_arrows: true,
+    // unsafe_comps: true,
+    // unsafe_math: true,
     booleans_as_integers: true,
   },
   mangle: {
    properties: {
-    // keep_quoted: true,
+    keep_quoted: true,
    },
+  },
+  format: {
+    wrap_func_args: false,
   },
 };
 
@@ -24,21 +28,27 @@ let js = readFileSync('src/main.js', 'utf8');
 // Some custom mangling of JS to assist / work around Terser
 js = js
   // Remove whitespace in CSS template literals
-  .replace(/`[^`]+`/g, tag => tag.replace(/\n+\s+/g, '').replace(/:\s+/g, ':'))
+  // .replace(/ = `[^`]+`/g, tag => tag.replace(/\s+/g, ''))
   // Replace const with let
-  // .replaceAll('const', 'let')
+  .replaceAll('const', 'let')
+  // Hoist for() vars to global (very risky) ~4B
+  .replaceAll('for(let ', 'for(')
   // Replace all strict equality comparison with abstract equality comparison
-  // .replaceAll('===', '==')
+  .replaceAll('===', '==')
   // Remove the last semicolon at the end of a CSS string
   .replaceAll(':0;`', ':0`');
 
 const minifiedJs = await minifyJs(js, options);
 
+const packed = cmdRegPack(minifiedJs.code, {
+  crushGainFactor: parseFloat(5),
+});
+
 const html = readFileSync('src/index.html', 'utf8');
 
 const inlined = html.replace(
   /<script[^>]*><\/script>/,
-  `<script>${minifiedJs.code}</script>`,
+  `<script>${packed}</script>`,
 );
 
 const minifiedInlined = minifyHtml(inlined, {
@@ -50,14 +60,19 @@ const mangled = minifiedInlined
   // .replaceAll('for(let ', 'for(') // Hoist for() vars to global (very risky) ~4B
   // .replaceAll('=>{let ', '=>{') // Hoist button onclick lets (very risky) ~8B
   // .replace('let t=get', 't=get') // Hoist getComputedStyle var (very risky) ~4B
-  // .replace('<!DOCTYPE html><html>', '') // Remove doctype & HTML opening tags
-  // .replace(';</script>', '</script>') // Remove final semicolon
-  // .replace('<head>', '') // Remove head opening tag
-  // .replace('</head>', '') // Remove head closing tag
-  // .replace('<body>', '') // Remove body opening tag
-  // .replace('</body></html>', ''); // Remove closing tags
+  // .replace('o=()=>', 'o=v=>') // Assign var to avoid ()
+  // .replace('oncanplay=()', 'oncanplay=w') // Assign var to avoid ()
+  // .replace('onclick=()', 'onclick=x') // Assign var to avoid ()
+  // .replace('onclick=()', 'onclick=y') // Assign var to avoid ()
+  // .replace('()=>Math.random', 'z=>Math.random') // Assign var to avoid ()
+  .replace('<!DOCTYPE html><html>', '') // Remove doctype & HTML opening tags
+  .replace(';</script>', '</script>') // Remove final semicolon
+  .replace('<head>', '') // Remove head opening tag
+  .replace('</head>', '') // Remove head closing tag
+  .replace('<body>', '') // Remove body opening tag
+  .replace('</body></html>', ''); // Remove closing tags
 
-console.log(`${mangled.length}B`);
+console.log(`Mangled: ${mangled.length}B`);
 
 writeFileSync('index.watch.html', minifiedInlined);
 
--- src/main.js
+++ src/main.js
@@ -16,7 +16,10 @@ const updateSlice = (index) => {
 
 const renderSlice = (index) => {
   const box = slices[index].firstChild;
-  const prevBox = slices[index - 1]?.firstChild;
+  // const prevBox = slices[index - 1] && slices[index - 1].firstChild;
+  const prevBoxX = slices[index - 1]?.firstChild.x || 0;
+  const prevBoxY = slices[index - 1]?.firstChild.y || 0;
+  const reflection = `${box.getHsla(55, 80)} 0, ${box.getHsla(55, 20)}`;
 
   slices[index].style.cssText = `
     grid-row: -${index};
@@ -37,8 +40,6 @@ const renderSlice = (index) => {
     transform: rotateX(60deg) rotateZ(45deg) translate(${box.x}vmin, ${box.y}vmin)
   `;
 
-  console.log(prevBox && box.y < prevBox.y);
-
   box.firstChild.style.cssText = `
     transform-style: preserve-3d;
     position: absolute;
@@ -50,7 +51,7 @@ const renderSlice = (index) => {
     background: linear-gradient(
       ${box.getHsla(55)} 50%,
       ${box.getHsla(70)} 0 calc(50% + 1px),
-      ${prevBox && box.y < prevBox.y ? `${box.getHsla(55, 80)} 0, ${box.getHsla(55, 20)}` : '#0000 0'}
+      ${box.y < slices[index - 1]?.firstChild.y || 0 ? reflection : '#0000 0'}
     )
   `;
 
@@ -65,7 +66,7 @@ const renderSlice = (index) => {
       -90deg,
       ${box.getHsla(60)} 50%,
       ${box.getHsla(70)} 0 calc(50% + 1px),
-      ${prevBox && box.x < prevBox.x ? `${box.getHsla(55, 80)} 0, ${box.getHsla(55, 20)}` : '#0000 0'}
+      ${box.x < slices[index - 1]?.firstChild.x || 0 ? reflection : '#0000 0'}
     )
   `;
 }
@@ -93,7 +94,10 @@ const addSlice = (width = 40, height = 40) => {
   slices.push(slice);
 }
 
-document.onclick = () => {
+const handleClick = (event) => {
+  if (event.key && event.key !== ' ') return;
+  event.preventDefault();
+
   const prevBox = slices.at(-2)?.firstChild;
   const currBox = slices.at(-1)?.firstChild;
   let width = 40;
@@ -116,6 +120,8 @@ document.onclick = () => {
   // Add a new slice
   addSlice(width, height);
 }
+
+document.onclick = document.onkeydown = handleClick;
 
 addSlice();
 renderSlice(0);
